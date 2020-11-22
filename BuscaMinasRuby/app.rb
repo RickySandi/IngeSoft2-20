ec5de10fd8b212169208a1296861b0d4c8664f74
RickySandi <50056806+RickySandi@users.noreply.github.com>
2020-11-22 19:25:44 -0400

    Add files via upload

---
+++ BuscaMinasRuby/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra'
+require './config'
+require './lib/Juego'
+
+juego = Juego.new
+
+get '/' do
+    #@celdas_Desbloqueadas = juego.getCeldasDesbloqueadas()
+    @matriz =juego.cargarMatriz()
+    erb:tablero
+end
+
+get '/abrirCasilla' do
+    x = params[:X].to_i
+    y = params[:Y].to_i
+    juego.seleccionarCelda(x,y)
+    @celdas_Desbloqueadas = '02'
+    @matriz =juego.cargarMatriz()
+    erb:tablero 
+end
